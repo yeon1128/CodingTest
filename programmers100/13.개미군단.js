5eb2dceec7f8fe9e753553a74a586715159055f5
jeongyeon.seo <s9530348@naver.com>
2022-10-20 01:17:56 +0900

    프로그래머스 LV.0 풀이 수정

--- programmers100/13.개미군단.js
+++ programmers100/13.개미군단.js
@@ -3,5 +3,18 @@
 // 일 = 1
 // 최소한의 병력
 
-// 5x + 3y + z = 23
-// 5로 나누고 3으로 나누고 나머지는 1로 처리
+/**
+ * hp / 5 = 몫
+ * hp % 5 = 나머지
+ * 장군 = Math.floor(hp / 5)
+ * 병정 = Math.floor(hp % 5 / 3)
+ * 일 = (hp % 5) % 3
+ */
+
+function solution(hp) {
+  let 장군 = Math.floor(hp / 5);
+  let 병정 = Math.floor((hp % 5) / 3);
+  let 일 = (hp % 5) % 3;
+
+  return 장군 + 병정 + 일;
+}
