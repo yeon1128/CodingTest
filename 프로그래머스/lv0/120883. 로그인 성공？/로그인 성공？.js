8d95db548fbcad555a74bf21766f00eff73e7a11
jeongyeon.seo <108205639+yeon1128@users.noreply.github.com>
2022-10-24 18:40:17 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,20 +1,16 @@
 function solution(id_pw, db) {
-    
-    let result = "";
-    
-    db.map(data => {
-        let check_id = id_pw[0] === data[0];
-        let check_pw = id_pw[1] === data[1];
-        
-        if(check_id && check_pw) {
-            return result = "login"
-        } else if (check_id && !check_pw){
-            return result = "wrong pw"
-        } else if (!check_id && !check_pw) {
-            return result = "fail"
-        }
-        
-    })
-    
-    return result;
+  let result = "";
+  db.map((data) => {
+    let checkedId = id_pw[0] === data[0];
+    let checkedPw = id_pw[1] === data[1];
+
+    if (checkedId && checkedPw) {
+      return (result = "login");
+    } else if (checkedId && !checkedPw) {
+      return (result = "wrong pw");
+    } else if (!checkedId && !checkedPw) {
+      return (result = "fail");
+    }
+  });
+  return result;
 }
