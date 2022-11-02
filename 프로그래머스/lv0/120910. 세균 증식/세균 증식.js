af42e1b5fb52a5014de91f37e3d05a47f97aa832
jeongyeon.seo <108205639+yeon1128@users.noreply.github.com>
2022-11-02 23:35:51 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
@@ -1,8 +1,6 @@
 function solution(n, t) {
   let result = 0;
-  for (let i = 0; i < t; i++) {
-    result += n * (2 ** i);
-  }
-  return result + n;
+  result += n * (2 ** t); 
+  return result;
 }
 
