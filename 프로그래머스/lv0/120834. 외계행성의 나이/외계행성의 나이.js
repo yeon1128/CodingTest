75cdfd90ec2e2868c2e1aa581badc6696819030c
jeongyeon.seo <108205639+yeon1128@users.noreply.github.com>
2022-11-15 15:47:44 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,12 +1,12 @@
 function solution(age) {
   const arr = String(age).split("");
   const alphabet = "abcdefghij".split("");
-  let answer = ""
+  let answer = "";
 
-  arr.forEach((v)=>{
+  arr.map((v) => {
     let index = Number(v);
     answer += alphabet[index];
-  })
-
-  return answer;
+  });
+    
+    return answer;
 }
