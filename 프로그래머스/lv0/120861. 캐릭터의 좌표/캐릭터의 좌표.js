396f6b7d3df03ce1b0c12ca0ee86efa081c72559
jeongyeon.seo <108205639+yeon1128@users.noreply.github.com>
2023-02-27 11:29:42 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,24 @@
+function solution(keyinput, board) {
+  let answer = [0, 0];
+  let xMax = Math.floor(board[0] / 2);
+  let yMax = Math.floor(board[1] / 2);
+
+  keyinput.forEach((e) => {
+    switch (e) {
+      case "left":
+        answer[0] !== -xMax && answer[0]--;
+        break;
+      case "right":
+        answer[0] !== xMax && answer[0]++;
+        break;
+      case "up":
+        answer[1] !== yMax && answer[1]++;
+        break;
+      case "down":
+        answer[1] !== -yMax && answer[1]--;
+        break;
+    }
+  });
+
+  return answer;
+}
